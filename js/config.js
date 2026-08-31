12b8fa93ac0730d6b3efc5c805dafc5c9d1abd8f
fieldflowapp <contacto@fieldflow.cl>
2026-08-31 17:48:28 -0400

    Fix public Apps Script endpoint for GitHub Pages

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.CTD_CONFIG = {
   TOURNAMENT_NAME: "Charles T. Darling Football Tournament 2026",
-  API_URL: "https://script.google.com/a/fieldflow.cl/macros/s/AKfycbzhTcXRRKVk_ztL3NmPcvOuUrjNSHlXsMPwDMgsu8mmi_q0n46LTvSuWZ0I-01hjBQX/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzhTcXRRKVk_ztL3NmPcvOuUrjNSHlXsMPwDMgsu8mmi_q0n46LTvSuWZ0I-01hjBQX/exec",
   REFRESH_SECONDS: 15,
   SCHOOL_LOGO: "https://drive.google.com/thumbnail?id=17GaYbEA3Nj54f0pueVQtphAfG05kObnp&sz=w1000",
   FIELDFLOW_LOGO: "https://drive.google.com/thumbnail?id=1F0ws2MvbVbn0R1G2qmgWKgz0P42BmZaB&sz=w1000",
