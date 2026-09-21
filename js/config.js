6fb385a4729d1100ef16ecf251116ea31b15fb32
fieldflowapp <contacto@fieldflow.cl>
2026-09-21 13:05:51 -0300

    Reconnect Apps Script API with fallback URLs

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window.CTD_CONFIG = {
   TOURNAMENT_NAME: "Charles T. Darling Football Tournament 2026",
   API_URLS: [
-    "https://script.google.com/macros/s/AKfycbw2GEuHniWpXM_knq6vwuITLPsclsD44RkpWbje8u8G1quVwCOlgq9aNAZvxxP3ZsYW/exec"
+    "https://script.google.com/macros/s/AKfycbw2GEuHniWpXM_knq6vwuITLPsclsD44RkpWbje8u8G1quVwCOlgq9aNAZvxxP3ZsYW/exec",
+    "https://script.google.com/a/macros/fieldflow.cl/s/AKfycbw2GEuHniWpXM_knq6vwuITLPsclsD44RkpWbje8u8G1quVwCOlgq9aNAZvxxP3ZsYW/exec"
   ],
   REFRESH_SECONDS: 15,
   SCHOOL_LOGO: "https://drive.google.com/thumbnail?id=17GaYbEA3Nj54f0pueVQtphAfG05kObnp&sz=w1000",
